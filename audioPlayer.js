8a5542258a0cbcdbcf646f826c8134f067a6f810
destro-1 <tjabbondanzio@gmail.com>
2024-03-15 14:17:48 -0400

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,77 +1,54 @@
-// audioPlayer.js
+//audioPlayer.js
 
-// Define an object to hold the audio player functionality
 const audioPlayer = {
-  audio: new Audio(), // Create an audio element
-  currentTrackIndex: 0,
-  playlist: [
-    { name: "Hit Em Up", src: "hitemup.mp3" },
-    { name: "Nollie Tre Flip", src: "Nollie Tre Flip.mp3" },
-    { name: "Everybody Sweep", src: "everybodysweep.mp3" },
+  audio: document.getElementById('audioPlayer'),
+  isPlaying: false,
+  currentSongIndex: 0,
+  songs: [
+    { title: "Hit Em Up", src: "hitemup.mp3" },
+    { title: "Nollie Tre Flip", src: "Nollie Tre Flip.mp3" },
+    { title: "Everybody Sweep", src: "everybodysweep.mp3" },
     // Add more songs as needed
   ],
-
-  // Function to initialize the audio player
-  initializeAudioPlayer: function() {
-    this.loadTrack();
-    this.audio.pause(); // Pause the audio by default
-    this.setupEventListeners();
+  
+  playPauseAudio: function() {
+    if (this.isPlaying) {
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+    this.isPlaying = !this.isPlaying;
   },
 
-  // Function to play the audio
-  playAudio: function() {
-    this.audio.play();
+  rewindAudio: function() {
+    this.audio.currentTime -= 10; // Rewind 10 seconds
   },
 
-  // Function to pause the audio
-  pauseAudio: function() {
-    this.audio.pause();
+  fastforwardAudio: function() {
+    this.audio.currentTime += 10; // Fast forward 10 seconds
   },
 
-  // Function to switch to the previous track
-  prevTrack: function() {
-    this.currentTrackIndex = (this.currentTrackIndex - 1 + this.playlist.length) % this.playlist.length;
-    this.loadTrack();
-    this.playAudio();
-  },
-
-  // Function to switch to the next track
-  nextTrack: function() {
-    this.currentTrackIndex = (this.currentTrackIndex + 1) % this.playlist.length;
-    this.loadTrack();
-    this.playAudio();
-  },
-
-  // Function to load the current track
-  loadTrack: function() {
-    this.audio.src = this.playlist[this.currentTrackIndex].src;
-  },
-
-  // Function to set up event listeners for the buttons
-  setupEventListeners: function() {
-    const playPauseButton = document.querySelector('.apple-music-button');
-    const prevButton = document.querySelector('.rewind-button');
-    const nextButton = document.querySelector('.fastforward-button');
-
-    playPauseButton.addEventListener('click', () => {
-      if (this.audio.paused) {
-        this.playAudio();
-      } else {
-        this.pauseAudio();
-      }
-    });
-
-    prevButton.addEventListener('click', () => this.prevTrack());
-    nextButton.addEventListener('click', () => this.nextTrack());
-
-    // Automatically switch to the next track when current track ends
-    this.audio.addEventListener('ended', () => {
-      this.nextTrack();
-    });
+  loadSong: function() {
+    const currentSong = this.songs[this.currentSongIndex];
+    this.audio.src = currentSong.src;
+    document.getElementById('songTitle').innerText = currentSong.title;
   }
 };
 
-// Add event listener to initialize the audio player when the DOM is loaded
-document.addEventListener('DOMContentLoaded', function() {
-  audioPlayer.initializeAudioPlayer();
+// Load the initial song
+audioPlayer.loadSong();
+
+// Update play/pause button icon based on audio playback state
+audioPlayer.audio.addEventListener('play', function() {
+  // Change play button to pause button
+  document.querySelector('.apple-music-button svg').innerHTML = `
+    <path d="M11 5H6v14h5v-14zm6 0h-5v14h5v-14z"/>
+  `;
+});
+
+audioPlayer.audio.addEventListener('pause', function() {
+  // Change pause button to play button
+  document.querySelector('.apple-music-button svg').innerHTML = `
+    <path d="M8 5v14l11-7z"/>
+  `;
 });
