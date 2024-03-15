8fd160d55e8dfee4cecf5dd0c19a8844d73be439
destro-1 <tjabbondanzio@gmail.com>
2024-03-15 13:20:32 -0400

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,50 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <script src="audioPlayer.js" defer></script>
-  <title>BUDDYSEVENUNDER</title>
-</head>
-<body>
-  <div class="circle-container">
-    <img src="ghostiepaint.png" alt="Circle Image">
-  </div>
-  <h1>Hello World!</h1>
-  <a href="https://www.google.com">This is Google</a>
-  <a href="https://www.google.com">Sign In</a>
+// audioPlayer.js
 
-  <!-- Buttons like Apple Music -->
-  <div class="player-controls">
-    <button class="rewind-button" onclick="audioPlayer.rewindAudio()">
-      <svg class="rewind-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M6 6h2v12H6zm3.5 6l8.5 6V6z"/>
-      </svg>
-    </button>
+// Define an object to hold the audio player functionality
+const audioPlayer = {
+  audio: null,
+  currentTrackIndex: 0,
+  playlist: [
+    { name: "Hit Em Up", src: "hitemup.mp3" },
+    { name: "Song 2", src: "song2.mp3" },
+    // Add more songs as needed
+  ],
 
-    <button class="apple-music-button" onclick="audioPlayer.playPauseAudio()">
-      <svg class="play-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M8 5v14l11-7z"/>
-      </svg>
-    </button>
+  // Function to initialize the audio player
+  initializeAudioPlayer: function() {
+    this.audio = document.getElementById('audioPlayer');
+    this.loadTrack();
+  },
 
-    <button class="fastforward-button" onclick="audioPlayer.fastforwardAudio()">
-      <svg class="fastforward-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M20 6v12h2V6h-2zm-14 6l8.5 6V6z"/>
-      </svg>
-    </button>
-  </div>
+  // Function to play or pause the audio
+  playPauseAudio: function() {
+    if (this.audio.paused) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+  },
 
-  <audio id="audioPlayer" src="hitemup.mp3"></audio>
+  // Function to rewind the audio track
+  rewindAudio: function() {
+    if (this.currentTrackIndex > 0) {
+      this.currentTrackIndex--;
+      this.loadTrack();
+      this.audio.play();
+    }
+  },
 
-  <!-- Script that controls Audio Player Buttons -->
-  <script>
-    document.addEventListener('DOMContentLoaded', function() {
-      audioPlayer.initializeAudioPlayer();
-    });
-  </script>
+  // Function to fast forward the audio track
+  fastforwardAudio: function() {
+    if (this.currentTrackIndex < this.playlist.length - 1) {
+      this.currentTrackIndex++;
+      this.loadTrack();
+      this.audio.play();
+    }
+  },
 
-  <h1>Buddy maybe make a sign in button</h1>
-</body>
-</html>
+  // Function to load the current track
+  loadTrack: function() {
+    this.audio.src = this.playlist[this.currentTrackIndex].src;
+  }
+};
+
+// Add event listener to initialize the audio player when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  audioPlayer.initializeAudioPlayer();
+});
